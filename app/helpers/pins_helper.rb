56bbef49dc3e005f7d80d5a0b5e13387716ca390
Amit Patel <amit.patel@botreeconsulting.com>
2015-06-02 17:18:53 +0530

    Added more log statement to debug the location issue.

--- app/helpers/pins_helper.rb
+++ app/helpers/pins_helper.rb
@@ -4,6 +4,8 @@ module PinsHelper
   end
 
   def pin_distance(pin)
+    Rails.logger.info "pin_distance>>>>>>>>>>>>> Pin : #{pin.id} - #{pin.name}"
+    Rails.logger.info "pin_distance>>>>>>>>>>>>> Co-ordinates : #{[@latitude, @longitude]}"
     if @latitude.present? && @longitude.present?
       "#{Geocoder::Calculations.to_kilometers(pin.distance_from([@latitude, @longitude])).round(2)} km"
     end
