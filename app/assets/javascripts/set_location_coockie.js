9d1bb69af951a9fb0300f9dd20ad224c2996f248
Olivier van Hees <oliviervanhees@gmail.com>
2015-07-20 22:41:45 +0200

    removed error message

--- app/assets/javascripts/set_location_coockie.js
+++ app/assets/javascripts/set_location_coockie.js
@@ -3,7 +3,7 @@ $( document ).ready(function() {
     // timeout at 60000 milliseconds (60 seconds)
     var options = {timeout:60000};
     geoLoc = navigator.geolocation;
-    watchID = geoLoc.watchPosition(showLocation, options);
+    watchID = geoLoc.watchPosition(showLocation, errorHandler, options);
   }else{
     alert("Sorry, browser does not support geolocation!");
   }
@@ -14,7 +14,8 @@ function showLocation(position) {
   set_coockie_location(position.coords.latitude, position.coords.longitude);
 }
 
-
+function errorHandler(err) {       
+}
 
 function set_coockie_location(latitude, longitude){
   document.cookie = "latitude = "+latitude+";";
