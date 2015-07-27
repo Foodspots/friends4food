81d67bef1d0f9686d0ab27cd056dca6c75e1f654
Olivier van Hees <oliviervanhees@gmail.com>
2015-07-28 01:20:04 +0200

    changed outgoing URL restaurants

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -92,6 +92,6 @@ class PinsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
-      params.require(:pin).permit(:description, :image, :name, :address, :place, :postal_code, :telephone_number, :website, :emailadress, :location, :latitude, :longitude, :search, :emailadress, :openinghours, :price, :category, :neighbourhood)
+      params.require(:pin).permit(:description, :image, :name, :address, :place, :postal_code, :telephone_number, :website, :emailadress, :location, :latitude, :longitude, :search, :emailadress, :openinghours, :price, :category, :neighbourhood, :kitchen)
     end
 end
