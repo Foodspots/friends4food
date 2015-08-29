9c4d38f0ce5827c23cfbb87720b6466960dce997
Tim van Dalen <Tim@timvdalen.nl>
2015-08-29 17:15:31 +0200

    Add share_snippet helper and display it on pages with friend list For #59

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
-  def pagination_class
-    'pagination-sm' if is_mobile_device?
-  end
+	def pagination_class
+		'pagination-sm' if is_mobile_device?
+	end
+
+	def share_snippet
+		"<div class='fb-send' data-href='#{Settings.app.share.url}' data-ref='share_direct'></div>".html_safe
+	end
 end
