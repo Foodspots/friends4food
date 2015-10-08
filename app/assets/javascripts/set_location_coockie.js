73cee0643ddc9ac9c4bb4119fb53e95d852c39a2
Tim van Dalen <Tim@timvdalen.nl>
2015-10-08 20:07:28 +0200

    Only ask for location permission after actually pressing the button Fixes #71

--- app/assets/javascripts/set_location_coockie.js
+++ app/assets/javascripts/set_location_coockie.js
@@ -1,15 +1,3 @@
-$( document ).ready(function() {
-  if(navigator.geolocation){
-    // timeout at 60000 milliseconds (60 seconds)
-    var options = {timeout:60000};
-    geoLoc = navigator.geolocation;
-    watchID = geoLoc.watchPosition(showLocation, errorHandler, options);
-  }else{
-    alert("Sorry, browser does not support geolocation!");
-  }
- 
-});
-
 function showLocation(position) {
   set_coockie_location(position.coords.latitude, position.coords.longitude);
 }
@@ -20,4 +8,19 @@ function errorHandler(err) {
 function set_coockie_location(latitude, longitude){
   document.cookie = "latitude = "+latitude+";";
   document.cookie = "longitude = "+longitude+";";
+
+	window.location = '/search_nearest_pins';
 }
+
+$(function() {
+	$(".ask_location").on('click', function () {
+		if(navigator.geolocation){
+			// timeout at 60000 milliseconds (60 seconds)
+			var options = {timeout:60000};
+			geoLoc = navigator.geolocation;
+			watchID = geoLoc.watchPosition(showLocation, errorHandler, options);
+		}else{
+			alert("Sorry, browser does not support geolocation!");
+		}
+	});
+});
