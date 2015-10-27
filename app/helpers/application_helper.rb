5ae030491ee00d385eea474d4fe4d66a407e1a8a
olivier van hees <oliviervanhees@gmail.com>
2015-10-27 23:03:49 +0100

    telephone number is click to call

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,11 @@ module ApplicationHelper
 	def share_snippet
 		"<div class='fb-send' data-href='#{Settings.app.share.url}' data-ref='share_direct'></div>".html_safe
 	end
+
+  def phone_number_link(text)
+    sets_of_numbers = text.scan(/[0-9]+/)
+    number = "#{sets_of_numbers.join('-')}"
+    link_to text, "tel:#{number}"
+  end
+
 end
