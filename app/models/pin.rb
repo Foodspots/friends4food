649b4563fa846229457e7f43e19e2461523ac91d
olivier van hees <oliviervanhees@gmail.com>
2015-11-24 20:37:25 +0100

    disabled location setting

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -4,8 +4,8 @@ class Pin < ActiveRecord::Base
   acts_as_votable
   belongs_to :user
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
-  before_save :geocode
-  geocoded_by :location, :if => :location_changed?
+  #before_save :geocode
+  #geocoded_by :location, :if => :location_changed?
 
 
   #def set_location
